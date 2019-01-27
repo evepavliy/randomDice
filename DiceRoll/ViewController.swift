d544117c4cf95c2fc5f7efe294469b97e3cd6cbf
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-01-27 22:55:31 +0100

    Initial commit for simple random dice generator

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func randomDice(_ sender: Any) {
+        
+        let randomNumber = Int.random(in: 1...6)
+        
+        label.text = "You Rolled : " + String(randomNumber)
+        imageView.image = UIImage.init(named: "Dice\(randomNumber)")
+    }
+    
 }
 
